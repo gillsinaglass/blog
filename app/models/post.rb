b05a97547d03f6c5c02f5246e628d4397bd84865
Ross Jones <rossmosis.jones@gmail.com>
2019-02-05 16:37:36 -0500

    added relations to models and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: "User"
+  belongs_to :reader, class_name: "User"
+  validates :title, presence: true
+  validates :title, length: {maximum: 60}
+  validates :description, presence: true
+  validates :description, length: {maximum: 200}
+  validates :content, presence: true
+  validates :content, length: {minimum: 100}
+  validate :author_check
+
+  def author_check
+    if !(self.author.id == self.author_id)
+      errors.add(:author, "You are not the Author")
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :readers, class_name: "Post", foreign_key: "reader_id"
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
