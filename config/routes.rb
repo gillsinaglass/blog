84435961bcdb6d09b475a582cdfca5f2f8e2910d
gillsinaglass <rdgill95@gmail.com>
2019-02-05 15:04:21 -0500

    Files created and resource run

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :users, only: %i(show edit new update create destroy)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
