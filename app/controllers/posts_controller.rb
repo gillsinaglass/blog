d0751c77fe15b7a2ca501e07343c6949203de0fb
gillsinaglass <rdgill95@gmail.com>
2019-02-05 15:24:47 -0500

    Controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,33 @@
 class PostsController < ApplicationController
+
+
+  def index
+    @post = Post.all
+  end
+
+  def new
+    @post = Post.new(post_params)
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to @post
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :description, :author_id, :reader_id)
+  end
+
+
 end
